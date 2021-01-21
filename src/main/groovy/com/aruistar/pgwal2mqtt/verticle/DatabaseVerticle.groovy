e690bc9d9a0938839286bb1d9aa6a955fb72e7b3
Liu Rui <lovearuis@gmail.com>
2021-01-21 13:51:39 +0800

    增加 了排除某些表的功能

--- src/main/groovy/com/aruistar/pgwal2mqtt/verticle/DatabaseVerticle.groovy
+++ src/main/groovy/com/aruistar/pgwal2mqtt/verticle/DatabaseVerticle.groovy
@@ -23,28 +23,44 @@ class DatabaseVerticle extends AbstractVerticle {
         def username = config.getString("username")
         def host = config.getString("host")
         def unique = config.getString("unique")
+        def slotName =config.getString("unique","test_slot")
         def port = config.getInteger("port")
+        List<String> exclude = config.getJsonArray("exclude", new JsonArray()).toList() as List<String>
+        List<String> include = config.getJsonArray("include", new JsonArray()).toList() as List<String>
 
-        def slotName = "test_slot"
+
+        def filterTables = exclude.join(",")
+        def addTables = include.join(",")
 
         "pg_recvlogical -h ${host} -p ${port} -U ${username} -d ${dbName} --slot ${slotName} --create-slot -P wal2json".execute()
 
-        Process.create(vertx, "pg_recvlogical",
-                [
-                        "-h", host,
-                        "-p", port.toString(),
-                        "-U", username,
-                        "-d", dbName,
-                        "--slot", slotName,
-                        "--start",
+        def params = [
+                "-h", host,
+                "-p", port.toString(),
+                "-U", username,
+                "-d", dbName,
+                "--slot", slotName,
+                "--start",
 //                        "-o", "pretty-print=true",
-                        "-o", "add-msg-prefixes=wal2json",
+                "-o", "add-msg-prefixes=wal2json",
 //                        "-o", "include-timestamp=true",
 //                        "-o", "include-type-oids=true",
 //                        "-o", "write-in-chunks=true",
-                        "-o", "format-version=1",
-                        "-f", "-"
-                ].toList()).start(process -> {
+                "-o", "format-version=1",
+                "-f", "-"
+        ]
+
+        if (exclude.size() > 0) {
+            params << "-o"
+            params << "filter-tables=${filterTables}".toString()
+        }
+
+        if (include.size() > 0) {
+            params << "-o"
+            params << "add-tables=${addTables}".toString()
+        }
+
+        Process.create(vertx, "pg_recvlogical", params.toList()).start(process -> {
             process.stdout().handler(buf -> {
 
                 buf.toString().eachLine {
