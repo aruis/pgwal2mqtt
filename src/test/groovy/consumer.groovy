3a17ee2f27bf36e9d39bf8ada4be0cbade8a3462
Liu Rui <lovearuis@gmail.com>
2021-01-21 09:51:26 +0800

    增加 一个订阅消息的demo

--- src/test/groovy/consumer.groovy
+++ src/test/groovy/consumer.groovy
@@ -1,1 +1,16 @@
+import io.vertx.core.Vertx
+import io.vertx.mqtt.MqttClient
 
+vertx = Vertx.vertx()
+MqttClient client = MqttClient.create(vertx);
+
+client.connect(31883, "192.168.0.88", ar -> {
+    println(ar.succeeded())
+
+    client.publishHandler({ s ->
+        System.out.println("There are new message in topic: " + s.topicName());
+        System.out.println("Content(as string) of the message: " + s.payload().toString());
+        System.out.println("QoS: " + s.qosLevel());
+    }).subscribe("pangu", 1);
+
+});
